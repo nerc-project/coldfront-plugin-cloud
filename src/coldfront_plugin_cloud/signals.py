c4a2482c16209d4dfe14ee81275f88bf609e758f
Kristi Nikolla <kristi@nikolla.me>
2022-08-12 15:36:34 -0400

    Email on task error
    Send an email when tasks encounter an exception.

--- src/coldfront_plugin_cloud/signals.py
+++ src/coldfront_plugin_cloud/signals.py
@@ -1,4 +1,5 @@
 import os
+import traceback
 
 from django.dispatch import receiver
 from django_q.tasks import async_task
@@ -12,6 +13,13 @@ from coldfront.core.allocation.signals import (allocation_activate,
                                                allocation_disable,
                                                allocation_remove_user,
                                                allocation_change_approved)
+from coldfront.core.utils.mail import send_email
+from coldfront.core.utils.common import import_from_settings
+
+EMAIL_ENABLED = import_from_settings('EMAIL_ENABLED', False)
+if EMAIL_ENABLED:
+    EMAIL_SENDER = import_from_settings('EMAIL_SENDER')
+    EMAIL_ADMIN_LIST = import_from_settings('EMAIL_ADMIN_LIST')
 
 
 def is_async():
@@ -21,6 +29,20 @@ def is_async():
     return os.getenv('REDIS_HOST')
 
 
+def run_fuction_and_email_on_error(func, *args):
+    try:
+        func(*args)
+    except Exception as e:
+        if EMAIL_ENABLED:
+            send_email(
+                "Error running task",
+                traceback.format_exc(),
+                EMAIL_SENDER,
+                [EMAIL_ADMIN_LIST]
+            )
+        raise e
+
+
 @receiver(allocation_activate)
 @receiver(allocation_change_approved)
 def activate_allocation_receiver(sender, **kwargs):
@@ -28,27 +50,29 @@ def activate_allocation_receiver(sender, **kwargs):
     # Note(knikolla): Only run this task using Django-Q if a qcluster has
     # been configured.
     if is_async():
-        async_task(activate_allocation, allocation_pk)
+        async_task(run_fuction_and_email_on_error, activate_allocation, allocation_pk)
     else:
-        activate_allocation(allocation_pk)
+        run_fuction_and_email_on_error(activate_allocation, allocation_pk)
 
 
 @receiver(allocation_disable)
 def allocation_disable_receiver(sender, **kwargs):
     allocation_pk = kwargs.get('allocation_pk')
-    disable_allocation(allocation_pk)
+    run_fuction_and_email_on_error(disable_allocation, allocation_pk)
 
 
 @receiver(allocation_activate_user)
 def activate_allocation_user_receiver(sender, **kwargs):
     allocation_user_pk = kwargs.get('allocation_user_pk')
     if is_async():
-        async_task(add_user_to_allocation, allocation_user_pk)
+        async_task(
+            run_fuction_and_email_on_error, add_user_to_allocation, allocation_user_pk
+        )
     else:
-        add_user_to_allocation(allocation_user_pk)
+        run_fuction_and_email_on_error(add_user_to_allocation, allocation_user_pk)
 
 
 @receiver(allocation_remove_user)
 def allocation_remove_user_receiver(sender, **kwargs):
     allocation_user_pk = kwargs.get('allocation_user_pk')
-    remove_user_from_allocation(allocation_user_pk)
+    run_fuction_and_email_on_error(remove_user_from_allocation, allocation_user_pk)
