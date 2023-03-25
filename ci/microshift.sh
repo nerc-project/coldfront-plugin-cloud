25652521a1f2cb4e36cbeb975673d41cafcea380
Lars Kellogg-Stedman <lars@redhat.com>
2023-03-25 08:04:51 -0400

    Group output lines
    Take advantage of GitHub workflow commands [1] to group related output from
    our tests. This reduces the amount of dross through which we have to search
    when reviewing CI results unless we explicitly need it to debug something.
    
    [1]: https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#grouping-log-lines

--- ci/microshift.sh
+++ ci/microshift.sh
@@ -13,16 +13,21 @@ mkdir -p "$test_dir"
 
 echo '127.0.0.1  onboarding-onboarding.cluster.local' | sudo tee -a /etc/hosts
 
+echo "::group::Start microshift container"
 sudo docker run -d --rm --name microshift --privileged \
     --network host \
     -v microshift-data:/var/lib \
     quay.io/microshift/microshift-aio:latest
+echo "::endgroup::"
 
+echo "::group::Start registry container"
 sudo docker run -d --name registry --network host registry:2
+echo "::endgroup::"
 
 KUBECONFIG_FULL_PATH="$(readlink -f "$KUBECONFIG")"
 mkdir -p "${KUBECONFIG_FULL_PATH%/*}"
 
+echo "::group::Wait for Microshift"
 for try in {0..10}; do
 	echo "copying kubeconfig {$try}"
 	sudo docker cp microshift:/var/lib/microshift/resources/kubeadmin/kubeconfig \
@@ -31,17 +36,23 @@ for try in {0..10}; do
 done
 
 while ! oc get route -A; do
-    echo "Waiting on Microshift"
+    echo "Waiting for Microshift"
     sleep 5
 done
+echo "::endgroup::"
 
 # Install OpenShift Account Management
 git clone "${ACCT_MGT_REPOSITORY}" "$test_dir/openshift-acct-mgt"
 git -C "$test_dir/openshift-acct-mgt" checkout "$ACCT_MGT_VERSION"
+
+echo "::group::Build openshift-acct-mgt image"
 sudo docker build "$test_dir/openshift-acct-mgt" -t "localhost:5000/cci-moc/openshift-acct-mgt:latest"
 sudo docker push "localhost:5000/cci-moc/openshift-acct-mgt:latest"
+echo "::endgroup::"
 
+echo "::group::Deploy openshift-acct-mgt"
 oc apply -k "$test_dir/openshift-acct-mgt/k8s/overlays/crc"
 oc wait -n onboarding --for=condition=available --timeout=800s deployment/onboarding
+echo "::endgroup::"
 
 sleep 60
