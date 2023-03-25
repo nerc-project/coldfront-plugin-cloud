3c544637f6b66d1c220c08d6079a3f6ce8979a32
Lars Kellogg-Stedman <lars@redhat.com>
2023-03-24 21:28:44 -0400

    Use test data directory instead of $HOME
    Place artifacts like the clone of the openshift-acct-mgt repository in a
    test data directory instead of using `$HOME`. This avoids cluttering the
    home directory of developers when they run these scripts locally.

--- ci/microshift.sh
+++ ci/microshift.sh
@@ -7,6 +7,10 @@ set -xe
 : "${ACCT_MGT_REPOSITORY:="https://github.com/cci-moc/openshift-acct-mgt.git"}"
 : "${KUBECONFIG:=$HOME/.kube/config}"
 
+test_dir="$PWD/testdata"
+rm -rf "$test_dir"
+mkdir -p "$test_dir"
+
 echo '127.0.0.1  onboarding-onboarding.cluster.local' | sudo tee -a /etc/hosts
 
 sudo docker run -d --rm --name microshift --privileged \
@@ -32,13 +36,12 @@ while ! oc get route -A; do
 done
 
 # Install OpenShift Account Management
-git clone "${ACCT_MGT_REPOSITORY}" ~/openshift-acct-mgt
-cd ~/openshift-acct-mgt
-git checkout "$ACCT_MGT_VERSION"
-sudo docker build . -t "localhost:5000/cci-moc/openshift-acct-mgt:latest"
+git clone "${ACCT_MGT_REPOSITORY}" "$test_dir/openshift-acct-mgt"
+git -C "$test_dir/openshift-acct-mgt" checkout "$ACCT_MGT_VERSION"
+sudo docker build "$test_dir/openshift-acct-mgt" -t "localhost:5000/cci-moc/openshift-acct-mgt:latest"
 sudo docker push "localhost:5000/cci-moc/openshift-acct-mgt:latest"
 
-oc apply -k k8s/overlays/crc
+oc apply -k "$test_dir/openshift-acct-mgt/k8s/overlays/crc"
 oc wait -n onboarding --for=condition=available --timeout=800s deployment/onboarding
 
 sleep 60
