964aa18a4f520e7132cd92c4a1ea3ef88fecc75e
Kristi Nikolla <kristi@nikolla.me>
2023-05-03 14:14:19 -0400

    Fix issue with length of names generated
    There was an extraneous `f` symbol which added 1 to the length.
    Added tests to verify the fix.
    
    Closes #96

--- src/coldfront_plugin_cloud/tests/unit/test_utils.py
+++ src/coldfront_plugin_cloud/tests/unit/test_utils.py
@@ -1,4 +1,7 @@
 import re
+import secrets
+from random import randrange
+
 from coldfront_plugin_cloud.tests import base
 from coldfront_plugin_cloud import utils
 
@@ -8,3 +11,11 @@ class TestGetSanitizedProjectName(base.TestBase):
         project_name = "---TEST - Software & Application Innovation Lab (SAIL) - TEST Projects   "
         sanitized_name = utils.get_sanitized_project_name(project_name)
         self.assertTrue(re.match(r"^[a-z0-9]([-a-z0-9]*[a-z0-9])?$", sanitized_name))
+
+    def test_get_unique_project_name_length(self):
+        project_name = secrets.token_hex(100)
+        max_length = randrange(50, 60)
+
+        self.assertGreater(len(project_name), max_length)
+        new_name = utils.get_unique_project_name(project_name, max_length=max_length)
+        self.assertEqual(len(new_name), max_length)
