39f26ec69ee2b1314fc45de5ff65fb3b531640f0
Kristi Nikolla <kristi@nikolla.me>
2023-09-07 11:43:23 +0200

    Refactor invoice writing logic
    - Now uses a dataclass
    - Same writing logic for both openshift and openstack.

--- src/coldfront_plugin_cloud/management/commands/calculate_storage_gb_hours.py
+++ src/coldfront_plugin_cloud/management/commands/calculate_storage_gb_hours.py
@@ -1,18 +1,52 @@
 import csv
+import dataclasses
 from datetime import datetime
 import logging
-import sys
 
 from coldfront_plugin_cloud import attributes
 from coldfront_plugin_cloud import utils
 
-from novaclient import client as novaclient
 from django.core.management.base import BaseCommand
 from coldfront.core.resource.models import Resource, ResourceType
 from coldfront.core.allocation.models import Allocation, AllocationStatusChoice
 import pytz
 
 logger = logging.getLogger(__name__)
+
+
+@dataclasses.dataclass
+class InvoiceRow:
+    Interval: str = ""
+    Project_Name: str = ""
+    Project_ID: str = ""
+    PI: str = ""
+    Invoice_Email: str = ""
+    Invoice_Address : str = ""
+    Institution: str = ""
+    Institution_Specific_Code: str = ""
+    Invoice_Type_Hours: int = ""
+    Invoice_Type: str = ""
+    Rate: str = ""
+    Cost: str = ""
+
+    @classmethod
+    def get_headers(cls):
+        """Returns all fields for display, with space instead of underscore."""
+        return [
+            field.name.replace("_", " ") for field in dataclasses.fields(cls)
+        ]
+
+    def get_value(self, field: str):
+        """Returns value for a field.
+
+        :param field: Field to return, with or without underscore.
+        """
+        return getattr(self, field.replace(" ", "_"))
+
+    def get_values(self):
+        return [
+            self.get_value(field) for field in self.get_headers()
+        ]
 
 
 def datetime_type(v):
@@ -29,6 +63,24 @@ class Command(BaseCommand):
                             help='End period for billing.')
 
     def handle(self, *args, **options):
+        def process_invoice_row(allocation, attribute, price):
+            """Calculate the value and write the bill using the writer."""
+            time = utils.calculate_quota_unit_hours(
+                allocation, attribute, options['start'], options['end']
+            )
+            billed = time * price
+            if billed > 0:
+                row = InvoiceRow(
+                    Interval=f"{options['start']} - {options['end']}",
+                    Project_ID=allocation.get_attribute(attributes.ALLOCATION_PROJECT_ID),
+                    PI=allocation.project.pi,
+                    Invoice_Type_Hours=billed,
+                    Invoice_Type=attr,
+                )
+                csv_invoice_writer.write(
+                    row.get_values()
+                )
+
         openstack_resources = Resource.objects.filter(
             resource_type=ResourceType.objects.get(
                 name='OpenStack'
@@ -51,53 +103,18 @@ class Command(BaseCommand):
                 f, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL
             )
             # Write Headers
-            csv_invoice_writer.writerow(
-                [
-                    "Interval",
-                    "Project Name",
-                    "Project ID",
-                    "PI",
-                    "Invoice Email",
-                    "Invoice Address",
-                    "Institution",
-                    "Institution Specific Code",
-                    "Invoice Type Hours",
-                    "Invoice Type",
-                    "Rate",
-                    "Cost",
-                ]
-            )
+            csv_invoice_writer.writerow(InvoiceRow.get_headers())
 
             for allocation in openstack_allocations:
                 allocation_str = f'{allocation.pk} of project "{allocation.project.title}"'
                 msg = f'Starting billing for for allocation {allocation_str}.'
                 logger.debug(msg)
 
-                for attr, price_per_unit in [
+                for attr, price in [
                     (attributes.QUOTA_VOLUMES_GB, 1),
                     (attributes.QUOTA_OBJECT_GB, 1)
                 ]:
-                    time = utils.calculate_quota_unit_hours(
-                        allocation, attr, options['start'], options['end']
-                    )
-                    billed = time * price_per_unit
-                    if billed > 0:
-                        csv_invoice_writer.writerow(
-                            [
-                                f"{options['start']} - {options['end']}",
-                                allocation.get_attribute(attributes.ALLOCATION_PROJECT_NAME),
-                                allocation.get_attribute(attributes.ALLOCATION_PROJECT_ID),
-                                allocation.project.pi,
-                                "",  # Invoice Email
-                                "",  # Invoice Address
-                                "",  # Institutions
-                                "",  # Institution Specific Code
-                                billed,
-                                attr,
-                                "",  # Rate
-                                "",  # Cost
-                            ]
-                        )
+                    process_invoice_row(allocation, attr, price)
 
             for allocation in openshift_allocations:
                 allocation_str = f'{allocation.pk} of project "{allocation.project.title}"'
@@ -107,24 +124,4 @@ class Command(BaseCommand):
                 for attr, price_per_unit in [
                     (attributes.QUOTA_LIMITS_EPHEMERAL_STORAGE_GB, 1)
                 ]:
-                    time = utils.calculate_quota_unit_hours(
-                        allocation, attr, options['start'], options['end']
-                    )
-                    billed = time * price_per_unit
-                    if billed > 0:
-                        csv_invoice_writer.writerow(
-                            [
-                                f"{options['start']} - {options['end']}",
-                                allocation.get_attribute(attributes.ALLOCATION_PROJECT_NAME),
-                                allocation.get_attribute(attributes.ALLOCATION_PROJECT_ID),
-                                allocation.project.pi,
-                                "",  # Invoice Email
-                                "",  # Invoice Address
-                                "",  # Institutions
-                                "",  # Institution Specific Code
-                                billed,
-                                attr,
-                                "",  # Rate
-                                "",  # Cost
-                            ]
-                        )
+                    process_invoice_row(allocation, attr, price)
