b70c4dc88f82eb96fdd190714d9f2f84bf43126d
QuanMPhm <qmpham2019@gmail.com>
2024-05-24 10:10:17 -0400

    Allow allocated project name to be displayed in Coldfront UI in the Project detail view

--- src/coldfront_plugin_cloud/config.py
+++ src/coldfront_plugin_cloud/config.py
@@ -1,7 +1,13 @@
 from coldfront.config.base import INSTALLED_APPS
 from coldfront.config.env import ENV
+from coldfront.config.core import ALLOCATION_ATTRIBUTE_VIEW_LIST
 
 if 'coldfront_plugin_cloud' not in INSTALLED_APPS:
     INSTALLED_APPS += [
         'coldfront_plugin_cloud',
+    ]
+
+if 'Allocated Project Name' not in ALLOCATION_ATTRIBUTE_VIEW_LIST:
+    ALLOCATION_ATTRIBUTE_VIEW_LIST += [
+        'Allocated Project Name'
     ]
